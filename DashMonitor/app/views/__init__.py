5408b126a808b99f7b8180e6d8a3788e55e5ff27
Henry BG <henry.blair@ssindex.org>
2024-06-04 17:40:14 -0400

    feat(app>view): ADD { configs, root }.py

--- DashMonitor/app/views/__init__.py
+++ DashMonitor/app/views/__init__.py
@@ -6,5 +6,29 @@ Here Layout and html Building will be defined
 # std imports
 
 # 3rd party imports
+from dash import html
 
 # local imports
+from DashMonitor.app.views.configs import (
+  EXTERNAL_SCRIPTS,
+  EXTERNAL_STYLESHEETS,
+  HTML_TITLE
+)
+
+from DashMonitor.app.views.root import MAIN_HTML
+
+
+
+ENTRY_LAYOUT = html.Div([])
+
+
+def setupView(app_settings={}):
+    app_settings = dict(
+        external_scripts = EXTERNAL_SCRIPTS,
+        external_stylesheets = EXTERNAL_STYLESHEETS,
+        index_string = MAIN_HTML,
+        title = HTML_TITLE,
+        **app_settings
+    )
+
+    return app_settings
