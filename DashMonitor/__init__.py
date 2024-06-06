7d3af00466399432d40a689fb5d99ce7ae1b92f0
Henry BG <henry.blair@ssindex.org>
2024-06-06 18:33:16 -0400

    docs(root>DashMonitor): IMPROVE im for __init__.py by adding docstring to main function and removing unnecesary if statement

--- DashMonitor/__init__.py
+++ DashMonitor/__init__.py
@@ -13,10 +13,9 @@ from DashMonitor.app import app
 
 
 def main():
+    '''
+    Main function to setup and config the execution of the app
+    '''
     print(f'Hello from {__name__}')
 
     app.run_server(debug=True, host="0.0.0.0", port=8052)
-
-
-if __name__ == "__main__":
-    ...
