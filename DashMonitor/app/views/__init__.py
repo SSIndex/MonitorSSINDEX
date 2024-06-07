9dddfa815e2dae2ecd3a4a3a9aea894e1410a7f1
Henry BG <henry.blair@ssindex.org>
2024-06-07 10:43:43 -0400

    feat(app>views): REPLACE hardcoded header with hader component from main.html

--- DashMonitor/app/views/__init__.py
+++ DashMonitor/app/views/__init__.py
@@ -17,8 +17,30 @@ from DashMonitor.app.views.configs import (
     HTML_TITLE,
 )
 
+from DashMonitor.app.views import components as cpt
 
-ENTRY_LAYOUT = html.Div([])
+
+ENTRY_LAYOUT = html.Div(
+    className='m-0 p-0',
+    children=list(
+        map(
+            lambda x: x.render(),
+            (
+                cpt.Header(
+                    'https://elasticbeanstalk-us-east-1-518344696083.s3.amazonaws.com/esg3-static-files/app/images/Branding2023/03+Isotipo/SVG/4-Negativo+Isotipo.svg',
+                    'ESG COMPASS',
+                    tabs=[
+                        cpt.HeaderTabBtn('general-analysis', 'GENERAL', True),
+                        cpt.HeaderTabBtn('sasb-analysis', 'SASB', False),
+                        cpt.HeaderTabBtn('ssindex-analysis', 'SSINDEX', False),
+                        cpt.HeaderTabBtn('geographic-analysis', 'MAP', False),
+                        cpt.HeaderTabBtn('benchmark-analysis', 'BENCHMARK', False),
+                    ],
+                ),
+            ),
+        )
+    ),
+)
 
 
 def setupView(app_settings={}):
