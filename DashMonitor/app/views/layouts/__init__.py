ced6e8c211ef8ed4572dfe609cf14a40f3cdf4e7
Henry BG <henry.blair@ssindex.org>
2024-06-25 17:11:48 -0400

    feat(views>layouts): ADD register_layouts method in __init__.py

--- DashMonitor/app/views/layouts/__init__.py
+++ DashMonitor/app/views/layouts/__init__.py
@@ -2,8 +2,29 @@
 Layouts Module. All Layouts will be added here
 '''
 
-from DashMonitor.app.views.layouts.benchmark_analysis import BENCHMARK_ANALYSIS_LAYOUT
+from DashMonitor.app.views.layouts.benchmark_analysis import (
+    BENCHMARK_ANALYSIS_LAYOUT,
+    register_layout_and_callbacks_benchmark,
+)
 from DashMonitor.app.views.layouts.general_analysis import GENERAL_ANALYSIS_LAYOUT
-from DashMonitor.app.views.layouts.geographic_analysis import GEOGRAPHIC_ANALYSIS_LAYOUT
-from DashMonitor.app.views.layouts.sasb_analysis import SASB_ANALYSIS_LAYOUT
-from DashMonitor.app.views.layouts.ssindex_analysis import SSINDEX_ANALYSIS_LAYOUT
+from DashMonitor.app.views.layouts.geographic_analysis import (
+    GEOGRAPHIC_ANALYSIS_LAYOUT,
+    register_layout_and_callbacks_map,
+)
+from DashMonitor.app.views.layouts.sasb_analysis import (
+    SASB_ANALYSIS_LAYOUT,
+    register_layout_and_callbacks_sasb,
+)
+from DashMonitor.app.views.layouts.ssindex_analysis import (
+    SSINDEX_ANALYSIS_LAYOUT,
+    register_layout_and_callbacks_ssindex,
+)
+
+
+def register_layouts_and_callbacks(app):
+    register_layout_and_callbacks_benchmark(app)
+    register_layout_and_callbacks_map(app)
+    register_layout_and_callbacks_sasb(app)
+    register_layout_and_callbacks_ssindex(app)
+
+    return app
