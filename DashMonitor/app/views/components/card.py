af887a2ceb3dfe90a25de0d06ed55290bda034ca
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2024-09-23 16:06:21 -0300

    fix(components/card): CHANGE to black formatting

--- DashMonitor/app/views/components/card.py
+++ DashMonitor/app/views/components/card.py
@@ -1,12 +1,12 @@
-
 from dash import html
 from DashMonitor.app.views.components.base_component import BaseComponent
 from typing import Optional
 
+
 class Card(BaseComponent):
     """
     Card Component. Generates a card component which displays information of the company and its overview/score.
-    
+
     Parameters
     ----------
     company_name : str
@@ -31,10 +31,19 @@ class Card(BaseComponent):
 
     _BASE_CARD_CLASS_NAME = 'card text-bg-primary'
 
-    def __init__(self, company_name: str, industry: str, country: str, company_image: str, 
-             overview: str, overview_text: str, overview_graph: BaseComponent, 
-             text_color: Optional[str] = None, background_color: Optional[str] = None):
-        
+    def __init__(
+        self,
+        company_name: str,
+        industry: str,
+        country: str,
+        company_image: str,
+        overview: str,
+        overview_text: str,
+        overview_graph: BaseComponent,
+        text_color: Optional[str] = None,
+        background_color: Optional[str] = None,
+    ):
+
         self.company_name = company_name
         self.industry = industry
         self.country = country
@@ -42,8 +51,14 @@ class Card(BaseComponent):
         self.overview = overview
         self.overview_text = overview_text
         self.overview_graph = overview_graph
-        self.class_name = f'card {background_color}' if background_color else self._BASE_CARD_CLASS_NAME
-        self.class_name = f'{self.class_name} {text_color}' if text_color else self.class_name
+        self.class_name = (
+            f'card {background_color}'
+            if background_color
+            else self._BASE_CARD_CLASS_NAME
+        )
+        self.class_name = (
+            f'{self.class_name} {text_color}' if text_color else self.class_name
+        )
 
     def render(self) -> html.Div:
         return html.Div(
@@ -53,10 +68,10 @@ class Card(BaseComponent):
                     className='card-body',
                     children=[
                         html.Div(
-                            className="row gx-5 pt-3 pb-3 ps-5 pe-5",
+                            className='row gx-5 pt-3 pb-3 ps-5 pe-5',
                             children=[
                                 html.Div(
-                                    className="col-6",
+                                    className='col-6',
                                     children=[
                                         html.Div(
                                             className='d-flex gap-4 align-items-center',
@@ -66,35 +81,52 @@ class Card(BaseComponent):
                                                         src=self.company_image,
                                                         className='rounded-circle img-fluid',
                                                         width='158px',
-                                                        alt='Company Image'
-                                                    )),
+                                                        alt='Company Image',
+                                                    )
+                                                ),
                                                 html.Div(
                                                     children=[
                                                         html.P(
-                                                            self.company_name, className='fs-3'),
+                                                            self.company_name,
+                                                            className='fs-3',
+                                                        ),
                                                         html.P(
-                                                            self.industry, className='fs-5'),
+                                                            self.industry,
+                                                            className='fs-5',
+                                                        ),
                                                         html.P(
-                                                            self.country, className='fs-5')
+                                                            self.country,
+                                                            className='fs-5',
+                                                        ),
                                                     ]
                                                 ),
-                                            ]
+                                            ],
                                         ),
                                         html.Div(
                                             children=[
-                                                html.P(children=[html.P(children=[html.Span(
-                                                    className="fw-bold",
-                                                    children=f"Overview: {self.overview}. "
+                                                html.P(
+                                                    children=[
+                                                        html.P(
+                                                            children=[
+                                                                html.Span(
+                                                                    className='fw-bold',
+                                                                    children=f'Overview: {self.overview}. ',
+                                                                ),
+                                                                self.overview_text,
+                                                            ]
+                                                        )
+                                                    ],
+                                                    className='mt-2',
                                                 ),
-                                                    self.overview_text])], className='mt-2'),
                                             ]
-                                        )],
+                                        ),
+                                    ],
                                 ),
                                 html.Div(
-                                    className="col-6 border rounded",
-                                    children=[self.overview_graph]
-                                )
-                            ]
+                                    className='col-6 border rounded',
+                                    children=[self.overview_graph],
+                                ),
+                            ],
                         )
                     ],
                 )
