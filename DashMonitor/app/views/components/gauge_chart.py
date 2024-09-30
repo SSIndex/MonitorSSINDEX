07dc411b10a7296642a53d9d1f1f6b3e3445709e
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2024-09-30 12:32:37 -0300

    feat(gauge_chart): HARD CODE mock_data for chart

--- DashMonitor/app/views/components/gauge_chart.py
+++ DashMonitor/app/views/components/gauge_chart.py
@@ -3,14 +3,44 @@ from dash import html
 
 class GaugeChart:
     # TODO: ADD DOCS and type hints, and refactor styles
+    mock_score = 44
+    mock_score_text = html.B(
+        className='text-ssindex-average',
+        children='Average',
+    )
+    mock_min_value = 0
+    mock_max_value = 100
+    mock_labels = [
+            html.B(className='text-ssindex-poor', children='Poor'),
+            html.B(className='text-ssindex-low', children='Low'),
+            html.B(className='text-ssindex-average', children='Average'),
+            html.B(className='text-ssindex-good', children='Good'),
+            html.B(className='text-ssindex-excellent', children='Excellent'),
+        ]
+
+    mock_score_labels = [
+        html.P(className='text-secondary', children='[0-19]'),
+        html.P(className='text-secondary', children='[20-39]'),
+        html.P(className='text-secondary', children='[40-59]'),
+        html.P(className='text-secondary', children='[60-70]'),
+        html.P(className='text-secondary', children='[80-100]'),
+    ]
 
     def __init__(self, score, score_text, min_value, max_value, labels, score_labels):
-        self.score = score
-        self.score_text = score_text
-        self.min_value = min_value
-        self.max_value = max_value
-        self.text_labels = labels
-        self.score_labels = score_labels
+        # TODO: when not using mock data, uncomment the following lines
+        # self.score = score
+        # self.score_text = score_text
+        # self.min_value = min_value
+        # self.max_value = max_value
+        # self.text_labels = labels
+        # self.score_labels = score_labels
+        self.score = self.mock_score
+        self.score_text = self.mock_score_text
+        self.min_value = self.mock_min_value
+        self.max_value = self.mock_max_value
+        self.text_labels = self.mock_labels
+        self.score_labels = self.mock_score_labels
+
 
     def calculate_position(self):
         """Calculate the percentage position of the score along the bar."""
