7d05c8cbb6d0e8f3b8b637bd461372b9b7f46d1d
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2024-09-30 10:33:35 -0300

    feat(geographic_analysis): ADD figma layout

--- DashMonitor/app/views/layouts/geographic_analysis.py
+++ DashMonitor/app/views/layouts/geographic_analysis.py
@@ -21,6 +21,11 @@ from DashMonitor.app.handlers.function_utils import (
     create_gauge_chart_ssindex,
 )
 
+# Import mock data
+from DashMonitor.app.views.layouts.mock_data_geographic_analysis import *
+
+from DashMonitor.app.views import components as cpt
+
 category_order = ["Universe", "Industry", "Company"]
 custom_colors = {
     "Universe": "#1f77b4",  # Blue
@@ -37,47 +42,31 @@ df = df[filtro_general].reset_index(drop=True)
 GEOGRAPHIC_ANALYSIS_LAYOUT = html.Div(
     className="container",
     children=[
+        # Date Picker Secion
         html.Section(
-            className="section bg-light pt-3",
+            className='section pt-3 d-flex justify-content-end',
+            children=[cpt.DatePicker().render()],
+        ),
+        html.Section(
+            className="section pt-5",
             children=[
                 html.Div(
-                    className="container border-bottom border-dark",
                     children=[
-                        html.Div(
-                            className="row",
+                        html.H4(
+                            className="text-primary", children=['Geographical Analysis']
+                        ),
+                        html.P(
+                            className='text-ssindex-graph-grey',
                             children=[
-                                html.H3(children=["Analisis Geografico Pilar SASB"])
+                                'Stakeholders feedback classified by territory'
                             ],
                         ),
-                        html.Div(
-                            className="row",
-                            children=[
-                                html.Div(
-                                    className="col-12",
-                                    style={
-                                        "display": "flex",
-                                        "flexDirection": "column",
-                                        "alignItems": "stretch",
-                                    },
-                                    children=dcc.Graph(id="general-map"),
-                                ),
-                            ],
-                        ),
-                        html.Div(
-                            className="row",
-                            children=[
-                                html.Div(
-                                    className="col-12",
-                                    style={
-                                        "display": "flex",
-                                        "flexDirection": "column",
-                                        "alignItems": "stretch",
-                                    },
-                                    children=html.Div(id="general-table"),
-                                )
-                            ],
-                        ),
-                    ],
+                        cpt.Table(
+                            headers=headers,
+                            data=data,
+                            class_name_headers=class_name_headers,
+                        ).render(),
+                    ]
                 )
             ],
         ),
@@ -85,82 +74,84 @@ GEOGRAPHIC_ANALYSIS_LAYOUT = html.Div(
 )
 
 
-def register_callbacks(app):
-    @app.callback(
-        [Output("general-map", "figure"), Output("general-table", "children")],
-        [Input("general-map", "clickData")],
-    )
-    def update_general_map(click_data):
-        filtered_df = df.copy()
-        filtered_df = filtered_df[filtered_df["Bank Name"] == bkn]
-        df_grouped = (
-            filtered_df.groupby("state").agg({"Sentiment_Score": "mean"}).reset_index()
-        )
-        df_grouped["color"] = df_grouped["Sentiment_Score"].apply(
-            lambda x: "blue" if x > 0 else "red"
-        )
 
-        fig = go.Figure(
-            data=go.Choropleth(
-                locations=df_grouped["state"],
-                z=df_grouped["Sentiment_Score"],
-                locationmode="USA-states",
-                colorscale="Blues",
-                marker_line_color="white",
-            )
-        )
-        fig.update_layout(
-            title_text=f"Average Sentiment Score by State for {bkn}",
-            geo_scope="usa",
-        )
-        reviews_table = html.Table()
-        if click_data:
-            state = click_data["points"][0]["location"]
-            reviews = filtered_df[filtered_df["state"] == state][
-                ["Review", "Sentiment_Score", "Bank Name"]
-            ].head(5)
-            reviews_table = html.Table(
-                [
-                    html.Thead(
-                        html.Tr(
-                            [
-                                html.Th(
-                                    col,
-                                    style={
-                                        "padding": "10px",
-                                        "border": "1px solid black",
-                                    },
-                                )
-                                for col in reviews.columns
-                            ]
-                        )
-                    ),
-                    html.Tbody(
-                        [
-                            html.Tr(
-                                [
-                                    html.Td(
-                                        reviews.iloc[i][col],
-                                        style={
-                                            "padding": "10px",
-                                            "border": "1px solid black",
-                                        },
-                                    )
-                                    for col in reviews.columns
-                                ]
-                            )
-                            for i in range(len(reviews))
-                        ]
-                    ),
-                ],
-                style={
-                    "width": "100%",
-                    "borderCollapse": "collapse",
-                    "marginTop": "20px",
-                },
-            )
-        return fig, reviews_table
+# def register_callbacks(app):
+#     @app.callback(
+#         [Output("general-map", "figure"), Output("general-table", "children")],
+#         [Input("general-map", "clickData")],
+#     )
+#     def update_general_map(click_data):
+#         filtered_df = df.copy()
+#         filtered_df = filtered_df[filtered_df["Bank Name"] == bkn]
+#         df_grouped = (
+#             filtered_df.groupby("state").agg({"Sentiment_Score": "mean"}).reset_index()
+#         )
+#         df_grouped["color"] = df_grouped["Sentiment_Score"].apply(
+#             lambda x: "blue" if x > 0 else "red"
+#         )
+
+#         fig = go.Figure(
+#             data=go.Choropleth(
+#                 locations=df_grouped["state"],
+#                 z=df_grouped["Sentiment_Score"],
+#                 locationmode="USA-states",
+#                 colorscale="Blues",
+#                 marker_line_color="white",
+#             )
+#         )
+#         fig.update_layout(
+#             title_text=f"Average Sentiment Score by State for {bkn}",
+#             geo_scope="usa",
+#         )
+#         reviews_table = html.Table()
+#         if click_data:
+#             state = click_data["points"][0]["location"]
+#             reviews = filtered_df[filtered_df["state"] == state][
+#                 ["Review", "Sentiment_Score", "Bank Name"]
+#             ].head(5)
+#             reviews_table = html.Table(
+#                 [
+#                     html.Thead(
+#                         html.Tr(
+#                             [
+#                                 html.Th(
+#                                     col,
+#                                     style={
+#                                         "padding": "10px",
+#                                         "border": "1px solid black",
+#                                     },
+#                                 )
+#                                 for col in reviews.columns
+#                             ]
+#                         )
+#                     ),
+#                     html.Tbody(
+#                         [
+#                             html.Tr(
+#                                 [
+#                                     html.Td(
+#                                         reviews.iloc[i][col],
+#                                         style={
+#                                             "padding": "10px",
+#                                             "border": "1px solid black",
+#                                         },
+#                                     )
+#                                     for col in reviews.columns
+#                                 ]
+#                             )
+#                             for i in range(len(reviews))
+#                         ]
+#                     ),
+#                 ],
+#                 style={
+#                     "width": "100%",
+#                     "borderCollapse": "collapse",
+#                     "marginTop": "20px",
+#                 },
+#             )
+#         return fig, reviews_table
 
 
 def register_layout_and_callbacks_map(app):
-    register_callbacks(app)
+    # register_callbacks(app)
+    pass
