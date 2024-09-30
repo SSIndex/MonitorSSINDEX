d2db104b23c98b14d6a2c3019eb9f7c1062ba690
Henry BG <henry.blair@ssindex.org>
2024-09-30 19:28:33 -0300

    feat(data>providers): UPDATE filestream_provider.py with preprocessing

--- DashMonitor/app/data/providers/filestream_provider.py
+++ DashMonitor/app/data/providers/filestream_provider.py
@@ -6,10 +6,14 @@ File Stream Provider Class Definition.
 from typing import List
 
 # # 3rd party imports
-from pandas import read_csv as pd_read_csv
+from pandas import (
+    read_csv as pd_read_csv,
+    to_datetime as pd_to_datetime
+)
 
 # local imports
 from DashMonitor.app.data.providers.base_provider import BaseProvider
+from DashMonitor.app.handlers.function_utils import categorize_score
 
 
 class FileStreamProvider(BaseProvider):
@@ -23,5 +27,30 @@ class FileStreamProvider(BaseProvider):
         self.path = path
         self.__df = pd_read_csv(path)
 
+        self._categorize_comments()
+        self._generate_year_month_columns()
+        self._generate_date_column()
+
+        print(self.__df.columns)
+
     def __call__(self):
         return self.__df.copy()
+    
+    def _categorize_comments(self):
+        self.__df['Sentiment_Category'] = self.__df["Sentiment_score"].apply(categorize_score)
+        self.__df["Normalized_Sentiment_Score"] = self.__df["Sentiment_score"]
+
+    def _generate_date_column(self):
+        if "DS" not in set(self.__df.columns):
+            self.__df["date"] = pd_to_datetime(
+                self.__df["year"].astype(str) + "-" + self.__df["month_num"].map("{:02}".format), format="%Y-%m"
+            )
+        else:
+            self.__df["date"] = self.__df["DS"].astype('datetime64[ns]')
+    
+    def _generate_year_month_columns(self):
+        if 'year' not in self.__df.columns and 'DS' in self.__df.columns:
+            self.__df['year'] = self.__df['DS'].astype('datetime64[ns]').dt.year
+        if 'month_num' not in self.__df.columns and 'DS' in self.__df.columns:
+            self.__df['month_num'] = self.__df['DS'].astype('datetime64[ns]').dt.month
+
