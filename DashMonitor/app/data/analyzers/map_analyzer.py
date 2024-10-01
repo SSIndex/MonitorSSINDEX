4c3d1e8e01d1e9143ca1c0e142aa1493654d702a
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2024-10-01 15:31:29 -0300

    feat(map_analyzer): ADD base map analyzer

---
+++ DashMonitor/app/data/analyzers/map_analyzer.py
@@ -0,0 +1,37 @@
+'''
+Map Analysys for Dataframe
+'''
+from DashMonitor.app.data.analyzers.base_analyzer import BaseAnalyzer
+
+class MapAnalyzer(BaseAnalyzer):
+    '''
+    MapAnalyzer generates all analysis related to Map Tab Specifically.
+    '''
+
+    def __init__(self, *a, **kw):
+        super().__init__(*a, **kw)
+        self.__data_analyzed = self._BaseAnalyzer__provider()
+
+    def _filter(self):
+        pass
+    
+    def _group(self):
+        df = self.__data_analyzed
+
+        self.__data_analyzed = (
+            df.groupby(["bank_name", "state"])[
+                ["sentiment_score"]
+            ]
+            .mean()
+            .reset_index()
+        )
+
+    def _aggregate(self): ...
+
+    def _having(self): ...
+
+    def _sort(self): ...
+
+    def execute(self):
+        super().execute()
+        return self.__data_analyzed
