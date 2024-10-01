d96aa371611695a61feaa09c15829dccf62b034a
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2024-10-01 12:11:57 -0300

    feat(card): ADD generate overview text with overview

--- DashMonitor/app/views/components/card.py
+++ DashMonitor/app/views/components/card.py
@@ -19,7 +19,7 @@ class Card(BaseComponent):
         URL of the Company Image. It must be a square image.
     overview : str
         Overview of the Company: Ex: 'Average'
-    overview_text : str
+    overview_text : Optional[str]
         Text of the Overview
     overview_graph : BaseComponent
         Graph of the Overview Score
@@ -38,8 +38,8 @@ class Card(BaseComponent):
         country: str,
         company_image: str,
         overview: str,
-        overview_text: str,
         overview_graph: BaseComponent,
+        overview_text: Optional[str] = None,
         text_color: Optional[str] = None,
         background_color: Optional[str] = None,
     ):
@@ -60,7 +60,22 @@ class Card(BaseComponent):
             f'{self.class_name} {text_color}' if text_color else self.class_name
         )
 
+    def _generate_overview_text(self) -> None:
+        if self.overview == 'Poor':
+            self.overview_text = ' Thieees company holds a low sentiment score. Feedback is mostly negative, with 80% of comments being negative and 20% positive. This indicates a negative perception among respondents'
+        
+        elif self.overview == 'Average':
+            self.overview_text = ' This company holds a medium sentiment score. Feedback is evenly split, with 50% of comments being positive and 50% negative. This indicates a balanced perception among respondents'
+        
+        elif self.overview == 'Good':
+            self.overview_text = ' This company holds a high sentiment score. Feedback is mostly positive, with 60% of comments being positive and 40% negative. This indicates a positive perception among respondents'
+        
+        elif self.overview == 'Excellent':
+            self.overview_text = ' This company holds a very high sentiment score. Feedback is overwhelmingly positive, with 80% of comments being positive and 20% negative. This indicates a very positive perception among respondents'
+
+    
     def render(self) -> html.Div:
+        self._generate_overview_text()
         return html.Div(
             className=self.class_name,
             children=[
