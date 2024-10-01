7c14f6b47eb76fb3d6690210b8768e08802f554a
Henry BG <henry.blair@ssindex.org>
2024-10-01 03:11:50 -0300

    feat(data>providers): UPDATE filestream_provider.py

--- DashMonitor/app/data/providers/filestream_provider.py
+++ DashMonitor/app/data/providers/filestream_provider.py
@@ -3,13 +3,11 @@ File Stream Provider Class Definition.
 '''
 
 # std imports
+import re
 from typing import List
 
 # # 3rd party imports
-from pandas import (
-    read_csv as pd_read_csv,
-    to_datetime as pd_to_datetime
-)
+from pandas import read_csv as pd_read_csv, to_datetime as pd_to_datetime
 
 # local imports
 from DashMonitor.app.data.providers.base_provider import BaseProvider
@@ -27,30 +25,38 @@ class FileStreamProvider(BaseProvider):
         self.path = path
         self.__df = pd_read_csv(path)
 
+        self._clean_columns()
         self._categorize_comments()
         self._generate_year_month_columns()
         self._generate_date_column()
 
-        print(self.__df.columns)
-
     def __call__(self):
         return self.__df.copy()
-    
+
+    def _clean_columns(self):
+        self.__df.columns = [
+            re.sub(r'\W+', '_', col.strip().lower()) for col in self.__df.columns
+        ]
+
     def _categorize_comments(self):
-        self.__df['Sentiment_Category'] = self.__df["Sentiment_score"].apply(categorize_score)
-        self.__df["Normalized_Sentiment_Score"] = self.__df["Sentiment_score"]
+        self.__df['sentiment_category'] = self.__df["sentiment_score"].apply(
+            categorize_score
+        )
+        self.__df["normalized_sentiment_score"] = self.__df["sentiment_score"]
 
     def _generate_date_column(self):
-        if "DS" not in set(self.__df.columns):
+        if "ds" not in set(self.__df.columns):
             self.__df["date"] = pd_to_datetime(
-                self.__df["year"].astype(str) + "-" + self.__df["month_num"].map("{:02}".format), format="%Y-%m"
+                self.__df["year"].astype(str)
+                + "-"
+                + self.__df["month_num"].map("{:02}".format),
+                format="%Y-%m",
             )
         else:
-            self.__df["date"] = self.__df["DS"].astype('datetime64[ns]')
-    
-    def _generate_year_month_columns(self):
-        if 'year' not in self.__df.columns and 'DS' in self.__df.columns:
-            self.__df['year'] = self.__df['DS'].astype('datetime64[ns]').dt.year
-        if 'month_num' not in self.__df.columns and 'DS' in self.__df.columns:
-            self.__df['month_num'] = self.__df['DS'].astype('datetime64[ns]').dt.month
+            self.__df["date"] = self.__df["ds"].astype('datetime64[ns]')
 
+    def _generate_year_month_columns(self):
+        if 'year' not in self.__df.columns and 'ds' in self.__df.columns:
+            self.__df['year'] = self.__df['ds'].astype('datetime64[ns]').dt.year
+        if 'month_num' not in self.__df.columns and 'ds' in self.__df.columns:
+            self.__df['month_num'] = self.__df['ds'].astype('datetime64[ns]').dt.month
