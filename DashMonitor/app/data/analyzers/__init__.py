5a091bc9b8a304e9efdeea8f6e6fd86a4fbcf574
Henry BG <henry.blair@ssindex.org>
2024-10-01 03:10:25 -0300

    feat(app>data): UPDATE __init__.py

--- DashMonitor/app/data/analyzers/__init__.py
+++ DashMonitor/app/data/analyzers/__init__.py
@@ -2,7 +2,8 @@
 Analyzers module. Specific 
 '''
 
-
-
 from DashMonitor.app.data.analyzers.base_analyzer import BaseAnalyzer
-from DashMonitor.app.data.analyzers.general_analyzer import GeneralAnalyzer
+from DashMonitor.app.data.analyzers.general_analyzer import (
+    GeneralAnalyzer,
+    GeneralComparisonAnalyzer,
+)
