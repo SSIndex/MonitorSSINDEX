1634c6d1b13c54d1b54dd11f9ba7b78f1a13663d
Henry BG <henry.blair@ssindex.org>
2024-10-01 03:13:07 -0300

    feat(data>analyzers): ADD { base_analyzer, general_analyzer }.py

--- DashMonitor/app/data/analyzers/general_analyzer.py
+++ DashMonitor/app/data/analyzers/general_analyzer.py
@@ -3,74 +3,134 @@ General Analysis for Dataframe
 '''
 
 from DashMonitor.app.data.analyzers.base_analyzer import BaseAnalyzer
-from numpy import isnan
+from DashMonitor.app.handlers.graphics_utils import (
+    CATEGORY_ORDER as gu_CATEGORY_ORDER,
+    SENTIMENT_SCORE_GROUPS as gu_SENTIMENT_SCORE_GROUPS,
+)
+from numpy import isnan as np_isnan
+from pandas import Categorical as pd_Categorical, DataFrame as pd_DataFrame
+
 
 class GeneralAnalyzer(BaseAnalyzer):
-  '''
-  GeneralAnalyzer generates all analysis related to General Tab Specifically.
-  '''
+    '''
+    GeneralAnalyzer generates all analysis related to General Tab Specifically.
+    '''
 
-  def __init__(self, *a, **kw):
-    super().__init__(*a, **kw)
-    self.__data_analyzed = self._BaseAnalyzer__provider()
+    def __init__(self, *a, **kw):
+        super().__init__(*a, **kw)
+        self.__data_analyzed = self._BaseAnalyzer__provider()
+
+    def _filter(self):
+        df = self.__data_analyzed
+
+        general_filter = (df["pilar"] == "Other") & (df["predicted_sasb"] == "Other")
+
+        self.__data_analyzed = df[general_filter].reset_index(drop=True)
+
+    def _group(self):
+        df = self.__data_analyzed
+
+        self.__data_analyzed = (
+            df.groupby(["bank_name", "predicted_pilar", "year", "month_num", "date"])[
+                ["sentiment_score"]
+            ]
+            .mean()
+            .reset_index()
+        )
+
+    def _aggregate(self):
+        df = self.__data_analyzed
+
+        df["total_sentiment_score"] = df.groupby(["bank_name", "date"])[
+            "sentiment_score"
+        ].transform("mean")
+
+    def _having(self): ...
+
+    def _sort(self):
+        self.__data_analyzed.sort_values("date", inplace=True)
+
+    def general_score(self, bank_name):
+        df = self.__data_analyzed
+
+        res = df[df["bank_name"] == bank_name]["total_sentiment_score"].mean()
+        return 0 if np_isnan(res) else res
+
+    def execute(self):
+        super().execute()
+
+        return self.__data_analyzed
 
 
-  def _filter_others(self):
-    df = self.__data_analyzed
-    
-    general_filter = (df["Pilar"] == "Other") & (df["Predicted_SASB"] == "Other")
+class GeneralComparisonAnalyzer(BaseAnalyzer):
 
-    self.__data_analyzed = df[general_filter].reset_index(drop=True)
-  
-  
-  def _group(self):
-    df = self.__data_analyzed
+    def __init__(self, industry_name, company_name, *a, **kw):
+        super().__init__(*a, **kw)
+        self.__company_name = company_name
+        self.__industry_name = industry_name
+        self.__data_analyzed = self._BaseAnalyzer__provider()
 
-    self.__data_analyzed = (
-        df.groupby(["Bank Name", "Predicted_Pilar", "year", "month_num", "date"])[
-            ["Sentiment_score"]
+    def _filter(self):
+        self.__data_industry = self.__data_analyzed[
+            self.__data_analyzed['industry'] == self.__industry_name
         ]
-        .mean()
-        .reset_index()
-    )
+        self.__data_company = self.__data_analyzed[
+            self.__data_analyzed['bank_name'] == self.__company_name
+        ]
 
-  def _agreggate(self):
-    df = self.__data_analyzed
+    def _group(self): ...
 
-    print(f'Columns: {df.columns}')
+    def _aggregate(self):
+        counts_analyzed = (
+            self.__data_analyzed["sentiment_category"].value_counts(normalize=True)
+            * 100
+        )
+        counts_industry = (
+            self.__data_industry["sentiment_category"].value_counts(normalize=True)
+            * 100
+        )
+        counts_company = (
+            self.__data_company["sentiment_category"].value_counts(normalize=True) * 100
+        )
 
-    df["Total_Sentiment_Score"] = df.groupby(["Bank Name", "date"])[
-        "Sentiment_score"
-    ].transform("mean")
-  
-  def _sort(self):
-    self.__data_analyzed.sort_values("date", inplace=True)
-  
-  def general_score(self, bank_name):
-    df = self.__data_analyzed
+        df = pd_DataFrame(
+            [
+                category_counts
+                for category in gu_SENTIMENT_SCORE_GROUPS
+                for category_counts in (
+                    {
+                        'Category': category,
+                        'Type': 'Universe',
+                        'Percentage': counts_analyzed.get(category, 0),
+                    },
+                    {
+                        'Category': category,
+                        'Type': 'Industry',
+                        'Percentage': counts_industry.get(category, 0),
+                    },
+                    {
+                        'Category': category,
+                        'Type': 'Company',
+                        'Percentage': counts_company.get(category, 0),
+                    },
+                )
+            ]
+        )
 
-    res = df[df["Bank Name"] == bank_name]["Total_Sentiment_Score"].mean()
-    return 0 if isnan(res) else res
+        df['Category'] = pd_Categorical(
+            df['Category'], categories=gu_SENTIMENT_SCORE_GROUPS, ordered=True
+        )
 
-  def execute(self):
-    self._filter_others()
-    self._group()
-    self._agreggate()
-    self._sort()
+        self.__data_histogram = df
 
-    return self.__data_analyzed
+    def _having(self): ...
 
+    def _sort(self):
+        self.__data_histogram.sort_values('Category')
 
-class IndustryAnalyzer(BaseAnalyzer):
+    def execute(self):
+        super().execute()
 
+        return self.__data_histogram
 
-  def __init__(self, industry_name, *a, **kw):
-    super().__init__(*a, **kw)
-    self.__data_analyzed = self._BaseAnalyzer__provider()
-    self.__data_analyzed = self.__data_analyzed[
-      self.__data_analyzed['Industry'] == industry_name
-    ]
-
-  def execute(self):
     ...
-  ...
