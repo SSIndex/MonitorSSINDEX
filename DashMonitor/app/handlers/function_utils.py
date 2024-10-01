513cf5dc996153b3f4af0e07e50562fb80b8b0da
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2024-10-01 12:10:06 -0300

    fix(function_utils): FIX label from Medium to Avrg

--- DashMonitor/app/handlers/function_utils.py
+++ DashMonitor/app/handlers/function_utils.py
@@ -19,7 +19,7 @@ def categorize_score(score):
     elif score <= 40:
         return "Low"
     elif score <= 60:
-        return "Medium"
+        return "Average"
     elif score <= 80:
         return "Good"
     else:
