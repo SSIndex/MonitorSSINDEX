c1a9cde2651c2c9f84a46509acd13d4496201db1
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2024-10-01 15:30:02 -0300

    feat(gauge_chart): USE score to text css class

--- DashMonitor/app/views/components/gauge_chart.py
+++ DashMonitor/app/views/components/gauge_chart.py
@@ -1,5 +1,6 @@
 from dash import html
 
+from DashMonitor.app.handlers.function_utils import categorize_score_to_text_class_name
 
 class GaugeChart:
     # TODO: ADD DOCS and type hints, and refactor styles
@@ -43,16 +44,17 @@ class GaugeChart:
 
     def _generate_score_text(self) -> None:
         """Generate the score text based on the score value."""
+        text_class_name = categorize_score_to_text_class_name(self.score)
         if self.score < 20:
-             self.score_text = html.B(className='text-ssindex-poor', children=self.score_text)
+             self.score_text = html.B(className=text_class_name, children=self.score_text)
         elif self.score < 40:
-            return html.B(className='text-ssindex-low', children=self.score_text)
+            return html.B(className=text_class_name, children=self.score_text)
         elif self.score < 60:
-            self.score_text = html.B(className='text-ssindex-average', children=self.score_text)
+            self.score_text = html.B(className=text_class_name, children=self.score_text)
         elif self.score < 80:
-             self.score_text = html.B(className='text-ssindex-good', children=self.score_text)
+             self.score_text = html.B(className=text_class_name, children=self.score_text)
         else:
-             self.score_text = html.B(className='text-ssindex-excellent', children=self.score_text)
+             self.score_text = html.B(className=text_class_name, children=self.score_text)
 
     def calculate_position(self):
         """Calculate the percentage position of the score along the bar."""
