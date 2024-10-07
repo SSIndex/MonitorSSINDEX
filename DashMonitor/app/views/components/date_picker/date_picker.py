2ee62e3e84a99db48b24661d4db0ead82931f3ea
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2024-10-07 16:09:58 -0300

    feat(date_picker): ADD disabled prop to component

--- DashMonitor/app/views/components/date_picker/date_picker.py
+++ DashMonitor/app/views/components/date_picker/date_picker.py
@@ -7,6 +7,11 @@ class DatePicker(BaseComponent):
     '''
     Date Picker Component. Generates a date picker component which allows the user to select a date
     and predefined ranges like 'Today', '5 days', etc.
+
+    Parameters
+    ----------
+    disabled : bool
+        If True, the Date Picker is disabled and cannot be interacted with. Default is False.
     '''
 
     # Constants for labels and button IDs
@@ -18,7 +23,22 @@ class DatePicker(BaseComponent):
     STYLE_SELECTED_BUTTON = 'btn btn-primary m-1'
     STYLE_UNSELECTED_BUTTON = 'btn btn-outline-primary m-1'
 
+    def __init__(self, disabled=False):
+        self.disabled = disabled
+
     def render(self):
+        # Determine the button classes and attributes based on the disabled state
+        button_class = (
+            'btn m-1 ms-2 border border-dark text-ssindex-graph-grey'
+            if not self.disabled
+            else 'btn m-1 ms-2 text-ssindex-graph-grey border border-0'
+        )
+
+        button_attributes = {
+            'data-bs-toggle': 'collapse',
+            'data-bs-target': '#date-picker-container',
+        } if not self.disabled else {}
+
         return html.Div(
             children=[
                 html.Label('Time Frame', className='form-label'),
@@ -26,13 +46,11 @@ class DatePicker(BaseComponent):
                 html.Button(
                     children=f'{date.today() - timedelta(days=365)} - {date.today()}',
                     id='date-picker-button',
-                    className='btn m-1 ms-2 border border-dark text-ssindex-graph-grey',
-                    **{
-                        'data-bs-toggle': 'collapse',
-                        'data-bs-target': '#date-picker-container',
-                    },
+                    className=button_class,
+                    disabled=self.disabled,
+                    **button_attributes,
                 ),
-                # Collapsible container for Date Picker
+                # Collapsible container for Date Picker (only renders if not disabled)
                 html.Div(
                     id='date-picker-container',
                     className='collapse z-3 position-absolute bg-white border rounded shadow-lg w-25',
@@ -86,6 +104,6 @@ class DatePicker(BaseComponent):
                             ],
                         )
                     ],
-                ),
+                ) if not self.disabled else None,
             ]
         )
