6130cfebb1e495b8fc74cc73b987c1c9998842f0
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2024-10-08 14:24:40 -0300

    fix(components/utils/button): IMPROVE MI with docs

--- DashMonitor/app/views/components/utils/button.py
+++ DashMonitor/app/views/components/utils/button.py
@@ -1,13 +1,24 @@
 class ButtonUtils:
     '''Utility class for handling button-related operations.'''
 
+    # Style classes for buttons
     STYLE_SELECTED_BUTTON = 'btn btn-primary m-1'
     STYLE_UNSELECTED_BUTTON = 'btn btn-outline-primary m-1'
 
     @staticmethod
     def selected_button_index(button_classes: list) -> int:
-        '''Get the index of the selected button. Returns -1 if no button is selected.'''
+        '''
+        Get the index of the selected button.
+
+        Iterates through the list of button classes to find the first occurrence
+        of the selected button style. Returns the index of the selected button,
+        or -1 if no button is currently selected.
+        '''
         return next(
-            (i for i, class_name in enumerate(button_classes) if ButtonUtils.STYLE_SELECTED_BUTTON in class_name),
+            (
+                i
+                for i, class_name in enumerate(button_classes)
+                if ButtonUtils.STYLE_SELECTED_BUTTON in class_name
+            ),
             -1,
         )
